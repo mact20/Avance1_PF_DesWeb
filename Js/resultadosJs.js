012e223df955960f114dce435bb790e026ddb611
mact20 <61257611+mact20@users.noreply.github.com>
2021-09-20 04:29:10 -0500

    Changes

--- Js/resultadosJs.js
+++ Js/resultadosJs.js
@@ -1,11 +1,14 @@
-var noPreguntas=localStorage.getItem("noPreguntas");
-var correctas=localStorage.getItem("correctas")
-
-var calculate_click = function(){
-    var nombre= $("#name").val();
-    localStorage.setItem("title",nombre);
-}
+var noPreguntas=parseFloat(localStorage.getItem("noPreguntas"));
+var correctas=parseFloat(localStorage.getItem("correctas"));
+var calificacion= correctas*100/noPreguntas;
 
 window.onload = function () {
-    $("#calculate").click(calculate_click);
+    $("#noP").text($("#noP").text()+noPreguntas);
+    $("#noC").text($("#noC").text()+correctas);
+    $("#grade").text($("#grade").text()+calificacion.toFixed(2));
 }
+
+/*var calculate_click = function(){
+    var nombre= $("#name").val();
+    localStorage.setItem("title",nombre);
+}*/
