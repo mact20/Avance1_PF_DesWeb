b69db10741f53ad4d1bd2227ed2d7bd47e8dad89
mact20 <61257611+mact20@users.noreply.github.com>
2021-09-20 11:59:06 -0500

    cambios

--- Js/examenJs.js
+++ Js/examenJs.js
@@ -1,3 +1,46 @@
+var correctas=0;
+var noPreguntas=5;
+
+var revisar_click = function(){
+    correctas=0;
+    for(j=1;j<=4;j++){
+        check("p"+j,"res"+j);
+    }
+    localStorage.setItem("correctas", correctas)
+    localStorage.setItem("noPreguntas", noPreguntas)
+}
+
+function check(name,res){
+    var elements=document.getElementsByName(name);
+    if(elements[0].type==="radio"){
+        for(i=0;i<4;i++){
+            var elementR=elements[i];
+                if(elementR.value==="correct" && elementR.checked){
+                    $("#"+res).text("Correcto");
+                    $("#"+res).css("color","green");
+                    correctas++;
+                    break;
+                }
+            if(i===3){
+                $("#"+res).text("Incorrecto");
+                $("#"+res).css("color","red");
+            }
+        }
+    }
+    if(elements[0].type==="text"){
+        if(elements[0].value==elements[1].value){
+            $("#"+res).text("Correcto");
+            $("#"+res).css("color","green");
+            correctas++;
+        }
+        else{
+            $("#"+res).text("Incorrecto");
+            $("#"+res).css("color","red");
+        }
+    }
+}
+
+
 function tituloExamen(){
     document.writeln("<h3>"+title+"</h3>");
 }
@@ -8,4 +51,9 @@ function preguntas(){
         document.writeln("Pregunta "+(i+1));
         document.writeln("</div>");
     }
+}
+
+
+window.onload = function () {
+    $("#revisar").click(revisar_click);
 }
