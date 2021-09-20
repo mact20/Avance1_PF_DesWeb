20e8e1701a66e7fe1a97d7235bc26c4e697b6be0
DafneDC <78834522+DafneDC@users.noreply.github.com>
2021-09-20 11:48:32 -0500

    Add files via upload

--- Js/resultadosJs.js
+++ Js/resultadosJs.js
@@ -1,14 +1,11 @@
-var noPreguntas=parseFloat(localStorage.getItem("noPreguntas"));
-var correctas=parseFloat(localStorage.getItem("correctas"));
-var calificacion= correctas*100/noPreguntas;
+var noPreguntas=localStorage.getItem("noPreguntas");
+var correctas=localStorage.getItem("correctas")
 
-window.onload = function () {
-    $("#noP").text($("#noP").text()+noPreguntas);
-    $("#noC").text($("#noC").text()+correctas);
-    $("#grade").text($("#grade").text()+calificacion.toFixed(2));
-}
-
-/*var calculate_click = function(){
+var calculate_click = function(){
     var nombre= $("#name").val();
     localStorage.setItem("title",nombre);
-}*/
+}
+
+window.onload = function () {
+    $("#calculate").click(calculate_click);
+}
