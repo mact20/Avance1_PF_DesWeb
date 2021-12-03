e20ef6829b886882c4f5c3687b5311a9bba855cb
Marcelo Sebastián Aguirre Alfaro <marcelo.sebastian97@gmail.com>
2021-12-02 23:22:29 -0600

    Cambios a Alumno.php
    muestra las cosas que debe con el usario que se loggea

--- alumno.php
+++ alumno.php
@@ -1,47 +1,36 @@
 <?php
+    include('../Avance1_PF_DesWeb/php/ConsultaBD.php');
+
     session_start();
 
     $mat = $_SESSION['matricula'];
-    include('ConsultaBD.php');
-    
-    $consultaNombre = "SELECT nombre FROM alumnos WHERE matricula = '$mat'";
-    $nombre = consultaBD($consultaNombre);
-    if(consultaBD($consultaNombre)){
-        $nombre = consultaBD($consultaNombre);
+        
+    $consulta = "SELECT * FROM alumnos WHERE matricula = '$mat'";
+    $result = consultaBD($consulta);
+
+    if(consultaBD($consulta)){
+        $row = mysqli_fetch_array($result);
     }
     else{
-        $nombre = 'error';
-    }
-
-    $consultaApellido = "SELECT apellido FROM alumnos WHERE matricula = '$mat'";
-    if(consultaBD($consultaApellido)){
-        $apellido = consultaBD($consultaApellido);
-    }
-    else{
-        $apellido = 'error';
-    }
-
-    $consultaGrado = "SELECT clase FROM alumnos WHERE matricula = '$mat'";
-    if(consultaBD($consultaGrado)){
-        $grado = consultaBD($consultaGrado);
-    }
-    else{
-        $grado = 'error';
+        echo "error";
     }
     
-    /*
-        $nombre = "Panchito";
-        $apellido = "Jimenez";
-        $grado = "5to";
-    */
+    $consulta2 = "SELECT alumnos.matricula, alumnos.nombre, grupos.idGrupo, materias.nombre, materias.idMateria
+        FROM alumnos 
+        JOIN inscritos on alumnos.matricula = inscritos.matAlumno
+        JOIN grupos on inscritos.idGrupo = grupos.idGrupo
+        JOIN materias on grupos.idMateria = materias.idMateria
+        WHERE alumnos.matricula = '$mat'";
+    $result2 = consultaBD($consulta2);
 
+    $array = array();
+    $index = 0;
 
-    $_SESSION['nombre'] = $nombre;
-    $_SESSION['apellido'] = $apellido;
-    $_SESSION['grado'] = $grado;
-    
-    
-
+    if(consultaBD($consulta2)){
+       $row2 = mysqli_fetch_array($result2);
+    }else{
+        echo "error 2";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -54,113 +43,98 @@
     </head>
     <body>
         <header>
-            <h1 class="header" ><?php echo "{$nombre} {$apellido}"; ?><img src="Images/logo.jpeg" alt="" id="logo"></h1>
+            <h1 class="header" ><img src="Images/logo.jpeg" alt="" id="logo"></h1>
         </header>
         <main>
             <h2>Informacion </h2>
             <ul>
-                <li>Nombre: <?php echo $nombre ?></li>
-                <li>Matricula: <?php echo $apellido ?></li>
-                <li>Grado:<?php echo $grado ?></li>
+                <li>Nombre: <?php echo $row['nombre']; ?> <?php echo $row['apellido']?></li>
+                <li>Matricula: <?php echo $row['matricula']; ?></li>
+                <li>Semestre:<?php echo $row['semestre']; ?></li>
             </ul>
+            
+
             <h2 style="text-align: center;">Examenes a realizar</h2>
             <section>
-                <div class="bloque_materia">
-                    <h3>Materia 1</h3>
-                    <ul>
-                        <li><a href="examen.html">Parcial 1</a></li>
-                        <li><a href="examen.html">Parcial 2</a></li>
-                        <li><a href="examen.html">Parcial 3</a></li>
-                    </ul>
-                    <p>Semestre Sep-Dic 2021</p>
-                </div>
-                <div class="bloque_materia">
-                    <h3>Materia 2</h3>
-                    <ul>
-                        <li><a href="examen.html">Parcial 1</a></li>
-                        <li><a href="examen.html">Parcial 2</a></li>
-                        <li><a href="examen.html">Parcial 3</a></li>
-                    </ul>
-                    <p>Semestre Sep-Dic 2021</p>
-                </div>
-                <div class="bloque_materia">
-                    <h3>Materia 3</h3>
-                    <ul>
-                        <li><a href="examen.html">Parcial 1</a></li>
-                        <li><a href="examen.html">Parcial 2</a></li>
-                        <li><a href="examen.html">Parcial 3</a></li>
-                    </ul>
-                    <p>Semestre Sep-Dic 2021</p>
-                </div>
+            <?php
+                $consulta2 = "SELECT alumnos.matricula, alumnos.nombre, grupos.idGrupo, materias.nombre, materias.idMateria
+                FROM alumnos 
+                JOIN inscritos on alumnos.matricula = inscritos.matAlumno
+                JOIN grupos on inscritos.idGrupo = grupos.idGrupo
+                JOIN materias on grupos.idMateria = materias.idMateria
+                WHERE alumnos.matricula = '$mat'";
+    
+                $result2 = consultaBD($consulta2);
+
+                while($row2 = mysqli_fetch_array($result2))
+                {
+                        
+                    $idMat = $row2['idMateria'];
+                    $consulta3 = "SELECT materias.nombre, examenesalumnos.nombreExamen, examenesalumnos.calificacion 
+                    FROM alumnos
+                    JOIN examenesalumnos ON alumnos.matricula = examenesalumnos.matAlumno
+                    JOIN examenes ON examenesalumnos.nombreExamen = examenes.nombre
+                    JOIN materias ON examenes.idMateria = materias.idMateria
+                    WHERE alumnos.matricula = '$mat' AND examenes.idMateria = '$idMat'";
+    
+                    $result3 = consultaBD($consulta3);
+
+                    echo"<div class='bloque_materia'>";
+                    echo"<h3>" . $row2['nombre'] . "</h3>
+                    
+                    <ul>"; 
+                    $index = 1;
+                    while($row3 = mysqli_fetch_array($result3)){
+                        echo "<li><a href='examen.php'> Parcial ". $index . "</a></li>";
+                        $index++;
+                    }  
+                    
+                    echo "</ul>
+                    <p>Grupo: " . $row2['idGrupo'] . "</p>
+                    </div>";  
+                }
+            ?>
             </section>
 
             <h2>Resultados</h2>
-                <div>
-                    <h3>Materia 1</h3>
-                    <table class="styled-table">
-                        <tr>
-                            <th>Examen</th>
-                            <th>Resultado</th>
-                        </tr>
-                        <tr>
-                            <td>Parcial 1</td>
-                            <td>85/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 2</td>
-                            <td>90/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 3</td>
-                            <td>90/100</td> 
-                        </tr>
-                    </table>
-                </div>
-
-                <div>
-                    <h3>Materia 2</h3>
-                    <table class="styled-table">
-                        <tr>
-                            <th>Examen</th>
-                            <th>Resultado</th>
-                        </tr>
-                        <tr>
-                            <td>Parcial 1</td>
-                            <td>85/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 2</td>
-                            <td>90/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 3</td>
-                            <td>90/100</td> 
-                        </tr>
-                    </table>
-                </div>
-
-                <div>
-                    <h3>Materia 3</h3>
-                    <table class="styled-table">
-                        <tr>
-                            <th>Examen</th>
-                            <th>Resultado</th>
-                        </tr>
-                        <tr>
-                            <td>Parcial 1</td>
-                            <td>85/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 2</td>
-                            <td>90/100</td> 
-                        </tr>
-                        <tr>
-                            <td>Parcial 3</td>
-                            <td>90/100</td> 
-                        </tr>
-                    </table>
-                </div>
-            
+            <?php      
+                $consulta2 = "SELECT alumnos.matricula, alumnos.nombre, grupos.idGrupo, materias.nombre, materias.idMateria
+                FROM alumnos 
+                JOIN inscritos on alumnos.matricula = inscritos.matAlumno
+                JOIN grupos on inscritos.idGrupo = grupos.idGrupo
+                JOIN materias on grupos.idMateria = materias.idMateria
+                WHERE alumnos.matricula = '$mat'";
+                
+                $result2 = consultaBD($consulta2);
+                      
+                while($row2 = mysqli_fetch_array($result2)){
+                    echo "<h3>".$row2['idGrupo']." ".$row2['nombre']."</h3>";
+                    echo "<table class='styled-table'>
+                    <tr>
+                        <th>Examen</th>
+                        <th>Resultado</th>
+                    </tr>";
+                    $idMat = $row2['idMateria'];
+                    $consulta3 = "SELECT materias.nombre, examenesalumnos.nombreExamen, examenesalumnos.calificacion 
+                    FROM alumnos
+                    JOIN examenesalumnos ON alumnos.matricula = examenesalumnos.matAlumno
+                    JOIN examenes ON examenesalumnos.nombreExamen = examenes.nombre
+                    JOIN materias ON examenes.idMateria = materias.idMateria
+                    WHERE alumnos.matricula = '$mat' AND examenes.idMateria = '$idMat'";
+    
+                    $result3 = consultaBD($consulta3);  
+                    while($row3 = mysqli_fetch_array($result3)){
+                        echo"
+                            <tr>
+                                <td>" . $row3['nombreExamen'] . "</td>
+                                <td>". $row3['calificacion'] ."</td> 
+                            </tr>";
+                        
+                    }
+                    echo "</table>";
+                           
+                }
+            ?>
         </main>
         <footer class="footer">
             <hr>
